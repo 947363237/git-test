db9300c490044d73b8bd5a79d7480684263ba7cb
Administrator <Administrator@XB-20170703WGXX>
2017-07-13 13:52:34 +0800

    w

--- src/ListFeatures.java
+++ src/ListFeatures.java
@@ -1,62 +1,62 @@
 //: holding/ListFeatures.java
+import typeinfo.pets.*;
 import java.util.*;
+import static net.mindview.util.Print.*;
 
-class Pet{}
-class Hamster{}
 public class ListFeatures {
   public static void main(String[] args) {
     Random rand = new Random(47);
     List<Pet> pets = Pets.arrayList(7);
-    System.out.println("1: " + pets);
+    print("1: " + pets);
     Hamster h = new Hamster();
     pets.add(h); // Automatically resizes
-    System.out.println("2: " + pets);
-    System.out.println("3: " + pets.contains(h));
+    print("2: " + pets);
+    print("3: " + pets.contains(h));
     pets.remove(h); // Remove by object
     Pet p = pets.get(2);
-    System.out.println("4: " +  p + " " + pets.indexOf(p));
+    print("4: " +  p + " " + pets.indexOf(p));
     Pet cymric = new Cymric();
-    System.out.println("5: " + pets.indexOf(cymric));
-    System.out.println("6: " + pets.remove(cymric));
+    print("5: " + pets.indexOf(cymric));
+    print("6: " + pets.remove(cymric));
     // Must be the exact object:
-    System.out.println("7: " + pets.remove(p));
-    System.out.println("8: " + pets);
+    print("7: " + pets.remove(p));
+    print("8: " + pets);
     pets.add(3, new Mouse()); // Insert at an index
-    System.out.println("9: " + pets);
+    print("9: " + pets);
     List<Pet> sub = pets.subList(1, 4);
-    System.out.println("subList: " + sub);
-    System.out.println("10: " + pets.containsAll(sub));
+    print("subList: " + sub);
+    print("10: " + pets.containsAll(sub));
     Collections.sort(sub); // In-place sort
-    System.out.println("sorted subList: " + sub);
+    print("sorted subList: " + sub);
     // Order is not important in containsAll():
-    System.out.println("11: " + pets.containsAll(sub));
+    print("11: " + pets.containsAll(sub));
     Collections.shuffle(sub, rand); // Mix it up
-    System.out.println("shuffled subList: " + sub);
-    System.out.println("12: " + pets.containsAll(sub));
+    print("shuffled subList: " + sub);
+    print("12: " + pets.containsAll(sub));
     List<Pet> copy = new ArrayList<Pet>(pets);
     sub = Arrays.asList(pets.get(1), pets.get(4));
-    System.out.println("sub: " + sub);
+    print("sub: " + sub);
     copy.retainAll(sub);
-    System.out.println("13: " + copy);
+    print("13: " + copy);
     copy = new ArrayList<Pet>(pets); // Get a fresh copy
     copy.remove(2); // Remove by index
-    System.out.println("14: " + copy);
+    print("14: " + copy);
     copy.removeAll(sub); // Only removes exact objects
-    System.out.println("15: " + copy);
+    print("15: " + copy);
     copy.set(1, new Mouse()); // Replace an element
-    System.out.println("16: " + copy);
+    print("16: " + copy);
     copy.addAll(2, sub); // Insert a list in the middle
-    System.out.println("17: " + copy);
-    System.out.println("18: " + pets.isEmpty());
+    print("17: " + copy);
+    print("18: " + pets.isEmpty());
     pets.clear(); // Remove all elements
-    System.out.println("19: " + pets);
-    System.out.println("20: " + pets.isEmpty());
+    print("19: " + pets);
+    print("20: " + pets.isEmpty());
     pets.addAll(Pets.arrayList(4));
-    System.out.println("21: " + pets);
+    print("21: " + pets);
     Object[] o = pets.toArray();
-    System.out.println("22: " + o[3]);
+    print("22: " + o[3]);
     Pet[] pa = pets.toArray(new Pet[0]);
-    System.out.println("23: " + pa[3].id());
+    print("23: " + pa[3].id());
   }
 } /* Output:
 1: [Rat, Manx, Cymric, Mutt, Pug, Cymric, Pug]
--- src/test/Test5.java
+++ src/test/Test5.java
@@ -1,17 +1,26 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Test;
 
 public class Test5 {
     public static void main(String[] args) {
-        HashMap<String, String> hashMap = new HashMap<String,String>();
-        hashMap.put("a", "ds");
-        hashMap.put("a", "ds");
-        hashMap.put("a", "ds");
-        hashMap.put("a", "ds");
-        hashMap.put("a", "ds");
-        hashMap.put("a", "ds");
-        hashMap.put("a", "ds");
-        System.out.println(hashMap);
+        ArrayList<Integer> list = new ArrayList<>();
+        Collections.addAll(list,1,2,3,4);
+        
+        List<Integer> subList = list.subList(0, 1);
+        System.out.println(subList);
+        
+        System.out.println(list.containsAll(subList));
+        
+        int a = 123;
+        int b = 12;
+        System.out.println();
     }
+    
 }
+
