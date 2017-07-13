23c2ff0714ce436bfe371c1fb86f80e23c3bff20
Administrator <Administrator@XB-20170703WGXX>
2017-07-13 17:30:56 +0800

    ewew

--- src/test/CollectionsTest.java
+++ src/test/CollectionsTest.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 import org.junit.Test;
 
@@ -14,24 +17,16 @@ public class CollectionsTest {
 
     @Test
     public void checkedCollection(){
-        ArrayList<Integer> arrayList = new ArrayList<Integer>();
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
-        arrayList.add(4);
+        LinkedList<Integer> linkedList = new LinkedList<Integer>(Arrays.asList(12,332,32,4,15));
+        Queue<Integer> priorityQueue = new PriorityQueue<Integer>(linkedList.size(), Collections.reverseOrder());
+        priorityQueue.addAll(linkedList);
         
-        
-        ArrayList<Integer> arrayList2 = new ArrayList<Integer>();
-        arrayList.add(12);
-        arrayList.add(22);
-        arrayList.add(32);
-        arrayList.add(42);
-        
-        ListIterator<Integer> listIterator = arrayList.listIterator(arrayList.size()-1);
-        while(listIterator.hasPrevious()){
-            arrayList2.add(listIterator.previous());
+        while(priorityQueue.peek()!=null){
+            System.out.print(priorityQueue.poll()+" ");
         }
         
-        System.out.println(arrayList2);
+        Integer[] arr = new Integer[]{1,22,223,4,5,6};
+        Arrays.sort(arr,Collections.reverseOrder());
+        System.out.println(Arrays.toString(arr));
     }
 }
