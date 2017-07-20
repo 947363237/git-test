1e52c61528f050aa237f02395a0bb671b55dcdd8
947363237 <947363237@qq.com>
2017-07-20 11:46:55 +0800

    w

--- src/test/StringTest.java
+++ src/test/StringTest.java
@@ -3,9 +3,12 @@ package test;
 import static net.mindview.util.Print.print;
 import static net.mindview.util.Print.printnb;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -78,6 +81,8 @@ public class StringTest {
         Pattern p = Pattern.compile("([a-zA-Z])|([0-9])");
         Matcher m = p.matcher(s);
         StringBuffer buf = new StringBuffer();
+        
+        //将所有匹配到的英文字母，替换成大写
         while(m.find()){
             if(m.group(1)!=null)
                 m.appendReplacement(buf,m.group().toUpperCase());
@@ -85,6 +90,56 @@ public class StringTest {
         }
         m.appendTail(buf);
         System.out.println(buf);
+    }
+    
+    @Test
+    public void test7(){
+        File absoluteFile = new File("src/test/StringTest.java").getAbsoluteFile();
+        System.out.println(absoluteFile);
+    }
+    
+    @Test
+    public void test8(){
+        String str = "lis 33 true";
+        Scanner scanner = new Scanner(str);
+        System.out.println("姓名："+scanner.next());
+        System.out.println("年龄："+scanner.nextInt());
+        System.out.println("男："+scanner.nextBoolean());
+    }
+    
+    @Test
+    public void test9(){
+        String str = "lis - 33 - true";
+        Scanner scanner = new Scanner(str);
+        scanner.useDelimiter("\\s*-\\s*");
+        while(scanner.hasNext()){
+            System.out.println(scanner.next());
+        }
+        Matcher matcher = scanner.delimiter().compile("\\s*-\\s*").matcher(str);
+        while(matcher.find()){
+            System.out.println(matcher.group());
+        }
+    }
+    
+    @Test
+    public void test10(){
+        String threatData =
+                "58.27.82.161@02/10/2005\n" +
+                "204.45.234.40@02/11/2005\n" +
+                "58.27.82.161@02/11/2005\n" +
+                "58.27.82.161@02/12/2005\n" +
+                "58.27.82.161@02/12/2005\n" +
+                "[Next log section with different data format]";
+        Scanner scanner = new Scanner(threatData);
+        String pattern = "(\\d+\\.\\d+\\.\\d+\\.\\d+)@(\\d+/\\d+/\\d+)";
+        while(scanner.hasNext(pattern)){
+            //String next = scanner.next(pattern);
+            scanner.next(pattern);
+            MatchResult match = scanner.match();
+            String ip = match.group(1);
+            String date = match.group(2);
+            System.out.println(ip+" : "+date);
+        }
     }
 }
 
